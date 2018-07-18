fac3cdcfdf7776913acf9ba0719376ee7c0cb9c3
Srujan Patel <srujanprophet@gmail.com>
2018-07-18 21:33:51 +0530

    Add python scripts

---
+++ video_pipeline.py
@@ -0,0 +1,5 @@
+from tensorflow_detection import DetectionObj
+
+if __name__ == '__main__':
+	detection = DetectionObj(model='ssd_mobilenet_v2_coco_11_06_2017')
+	detection.video_pipeline(video="./sample_videos/ducks.mp4", audio=False)
